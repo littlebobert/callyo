cf7f7a4f9fd849bb9c9834b9ca82fec41110f0aa
Justin Garcia <justin@justins-macbook-pro.local>
2018-03-22 15:53:08 -0400

    Initial Commit

---
+++ callyo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  callyo
+//
+//  Created by Justin Garcia on 3/22/18.
+//  Copyright © 2018 Justin Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ callyo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  callyo
+//
+//  Created by Justin Garcia on 3/22/18.
+//  Copyright © 2018 Justin Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
